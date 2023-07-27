032a32258d6cf12bb298149dde2b17726d33152a
Juris-Berkulis <krztfjrbhm1@gmail.com>
2023-07-27 12:56:12 +0300

    1. Make each message a separate component.

---
+++ src/components/BaseMessage.vue
@@ -0,0 +1,67 @@
+<script setup>
+import messagesList from '../assets/data/feed.json';
+
+const highlightByColor = (strPart, tone) => {
+    return `<mark style="background-color: rgb(${tone < 0 ? `255, ${(1 + tone) * 255}` : `${(1 - tone) * 255}, 255`}, 0);">${strPart}</mark>`
+};
+
+const convertString = (str, pointsList) => {
+    let strIndex = 0;
+    let newStr = '';
+
+    pointsList.forEach(pointItem => {
+        newStr += str.slice(strIndex, pointItem.position);
+        strIndex = pointItem.position + pointItem.length;
+        const strPart = str.slice(pointItem.position, strIndex);
+        newStr += highlightByColor(strPart, pointItem.tone);
+    });
+
+    newStr += str.slice(strIndex);
+
+    return newStr
+};
+</script>
+
+<template>
+<div class="messageItem" v-for="message of messagesList">
+    <p class="messageInfo">
+        <span>{{ message.date }}</span>
+        <span>/ {{ message.authorName }} /</span>
+        <span>{{ message.authorUrl }}</span>
+    </p>
+    <p class="messageText" v-html="convertString(message.content, message.contentPostTones)"></p>
+</div>
+</template>
+
+<style scoped>
+.messageItem {
+    margin: 0 30px;
+    padding: 30px 0;
+    border-bottom: 1px solid #000000;
+    font-size: 1rem;
+}
+
+.messageItem:last-child {
+    border: none;
+}
+
+.messageInfo {
+    margin-bottom: 30px;
+}
+
+.messageInfo span:first-child {
+    margin-right: 5px;
+}
+
+.messageInfo span:nth-child(2) {
+    font-weight: 700;
+}
+
+.messageInfo span:last-child {
+    margin-left: 5px;
+}
+
+.messageText {
+    line-height: 1.25;
+}
+</style>
--- src/components/TheFeed.vue
+++ src/components/TheFeed.vue
@@ -1,39 +1,10 @@
 <script setup>
-import messagesList from '../assets/data/feed.json';
-
-console.log(messagesList)
-
-const highlightByColor = (strPart, tone) => {
-    return `<mark style="background-color: rgb(${tone < 0 ? `255, ${(1 + tone) * 255}` : `${(1 - tone) * 255}, 255`}, 0);">${strPart}</mark>`
-};
-
-const convertString = (str, pointsList) => {
-    let strIndex = 0;
-    let newStr = '';
-
-    pointsList.forEach(pointItem => {
-        newStr += str.slice(strIndex, pointItem.position);
-        strIndex = pointItem.position + pointItem.length;
-        const strPart = str.slice(pointItem.position, strIndex);
-        newStr += highlightByColor(strPart, pointItem.tone);
-    });
-
-    newStr += str.slice(strIndex);
-
-    return newStr
-};
+import BaseMessage from '@/components/BaseMessage.vue';
 </script>
 
 <template>
 <div class="messagesList">
-    <div class="messageItem" v-for="message of messagesList">
-        <p class="messageInfo">
-            <span>{{ message.date }}</span>
-            <span>/ {{ message.authorName }} /</span>
-            <span>{{ message.authorUrl }}</span>
-        </p>
-        <p class="messageText" v-html="convertString(message.content, message.contentPostTones)"></p>
-    </div>
+    <BaseMessage />
 </div>
 </template>
 
@@ -42,36 +13,5 @@ const convertString = (str, pointsList) => {
     max-width: 1230px;
     margin: 0 auto;
     padding: 0 15px;
-}
-
-.messageItem {
-    margin: 0 30px;
-    padding: 30px 0;
-    border-bottom: 1px solid #000000;
-    font-size: 1rem;
-}
-
-.messageItem:last-child {
-    border: none;
-}
-
-.messageInfo {
-    margin-bottom: 30px;
-}
-
-.messageInfo span:first-child {
-    margin-right: 5px;
-}
-
-.messageInfo span:nth-child(2) {
-    font-weight: 700;
-}
-
-.messageInfo span:last-child {
-    margin-left: 5px;
-}
-
-.messageText {
-    line-height: 1.25;
 }
 </style>
